2a966dfa4008a81ec7331de54a21d88223184e1c
anggiie <anggiie02>
2021-06-05 17:09:06 +0200

    first commit & add HomePage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './HomePage.css'
+
+const HomePage = () => {
+    return (
+        <>
+            <h1>Welcome</h1>
+            <p>to</p>
+            <h2>GROUPOMANIA</h2>
+        </>
+    )
+}
+
+export default HomePage;
