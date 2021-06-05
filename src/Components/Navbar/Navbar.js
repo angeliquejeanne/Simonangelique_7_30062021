c59cd52bde8f2ae0dc0e2ae6fb9e68768ab1bf91
anggiie <anggiie02>
2021-06-05 20:09:17 +0200

    add modifs

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,8 +4,15 @@ import './Navbar.css'
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <button>Login</button>
-            <button>SignUp</button>
+            <div className="left">
+                <p className="logo">
+                    GROUPOMANIA
+                </p>
+            </div>
+            <div className="right">
+                <button>Login</button>
+                <button>SignUp</button>
+            </div>
         </nav>
     )
 }
