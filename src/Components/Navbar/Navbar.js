44a32fad96b356b7b622ca45734de0bb8e66a35c
anggiie <anggiie02>
2021-06-05 20:27:08 +0200

    add logo Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import './Navbar.css'
+import logoleftfont from '../../Assets/images/logo-monochrome-white.svg'
 
 export default function Navbar() {
     return (
         <nav className="navbar">
             <div className="left">
-                <p className="logo">
-                    GROUPOMANIA
-                </p>
+                <img className="logo" alt="logo" src={logoleftfont} />
             </div>
             <div className="right">
                 <button>Login</button>
