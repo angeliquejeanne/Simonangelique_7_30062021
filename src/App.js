a514bfe0e5a3a0bad1520a2c5f3e44f8e84b8517
anggiie <anggiie02>
2021-06-05 21:43:50 +0200

    add modifs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './Pages/Home/Home'
 import Login from './Components/AuthForm/Login'
 import SignUp from './Components/AuthForm/SignUp'
+import logoleftfont from './Assets/images/icon.svg'
 
 function App() {
   return (
