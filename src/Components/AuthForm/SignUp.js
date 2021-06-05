8e4c4396c594098a904ecb24be2345eaa8ff01aa
anggiie <anggiie02>
2021-06-06 00:50:38 +0200

    add modifs

--- src/Components/AuthForm/SignUp.js
+++ src/Components/AuthForm/SignUp.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react'
+import React, { useRef, useState, useContext } from 'react'
 import './AuthForm.css'
 import { useSelector, useDispatch } from 'react-redux'
 
